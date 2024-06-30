52fe81e3b4741ecf186f94c613e5f8b442763d9c
Ksenija <omega-2077@list.ru>
2024-06-30 21:38:44 +0300

    change task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+int account = 200;
+int payment = 1300;
+int bonus = payment>1000 ? payment/100 : 0;
+
+int total = account + payment + bonus;
+
+        System.out.println("Итоговый счет:" + total);
     }
 }
