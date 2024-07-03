c90caa1aea24c3d6295e7a499579e1bd5d7e0d8b
Ksenija <omega-2077@list.ru>
2024-07-03 14:09:18 +0300

    change 11

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-int account = 200;
-int payment = 800;
-int bonus = payment>1000 ? payment/100 : 0;
+        int account = 300;
+        int payment = 1200;
+        int bonus = payment > 1000 ? payment / 100 : 0;
 
-int total = account + payment + bonus;
+        int total = account + payment + bonus;
 
         System.out.println("Итоговый счет:" + total);
     }
